12d71260fdcd82e6cc82857913b0af3a408b7536
Gloria Michel Rodriguez Morteo <L23350607@tuxtepec.tecnm.mx>
2025-10-15 20:59:40 -0600

    menu.js y cambio de tamaño

---
+++ ALEJANDRAGUZMAN/js/menu.js
@@ -0,0 +1,12 @@
+(function(){
+    const openBtn=document.querySelector('.nav-menu');
+    const menu=document.querySelector('.nav-link');
+    const closeMenu=document.querySelector('.nav-close');
+
+    openBtn.addEventListener('click', ()=>{
+        menu.classList.add('nav-link--show');
+    });
+    closeMenu.addEventListener('click',()=>{
+        menu.classList.remove('nav-link--show');
+    })
+})();
